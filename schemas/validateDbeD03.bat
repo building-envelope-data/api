397df71dd7f52ef2a8f49cec904eeb540420aacd
Christoph Maurer <christoph.maurer@ise.fraunhofer.de>
2020-07-08 13:10:33 +0200

    validateDbeD03.bat and validateDsbD03.bat close the console right after the first ajv command, but the result of ajv can be directed to validateDbeResult.txt and validateDsbResult.txt.

---
+++ schemas/validateDbeD03.bat
@@ -0,0 +1,9 @@
+REM single validation of dbe
+
+echo Test0004
+
+PING localhost -n 3 >NUL
+
+ajv compile -s dbe.json -r calorimetric.json -r common.json -r component.json -r gaps.json -r geometry.json -r hygrothermal.json -r identifier.json -r layer.json -r material.json -r method.json -r number.json -r optical.json -r photovoltaic.json -r stakeholder.json -r string.json -r unit.json > validateDbeResult.txt
+
+PING localhost -n 10 >NUL
