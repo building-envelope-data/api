e59e669e99012d56785d8410ff6be1e744c21b07
Christoph Maurer <christoph.maurer@ise.fraunhofer.de>
2020-07-08 13:29:01 +0200

    validateSchemasD04.bat should be executed before each commit to validate dbe and dsb.

---
+++ schemas/validateDbeD04.bat
@@ -0,0 +1,7 @@
+REM validation of dbe
+
+echo TestD04
+
+ajv compile -s dbe.json -r calorimetric.json -r common.json -r component.json -r gaps.json -r geometry.json -r hygrothermal.json -r identifier.json -r layer.json -r material.json -r method.json -r number.json -r optical.json -r photovoltaic.json -r stakeholder.json -r string.json -r unit.json
+
+PING localhost -n 10 >NUL
