e2f68f4f5527ff0e953341430f3e1fa451ce960b
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2020-08-11 10:48:20 +0200

    Extract data-only schemas from `schemas/optical.json` and `schemas/calorimetric.json`

--- schemas/validateDsbD04.bat
+++ schemas/validateDsbD04.bat
@@ -2,6 +2,6 @@ REM validation of dsb
 
 echo TestD04
 
-ajv compile -s dsb.json -r dbe.json -r calorimetric.json -r common.json -r component.json -r gaps.json -r geometry.json -r hygrothermal.json -r identifier.json -r layer.json -r material.json -r method.json -r number.json -r optical.json -r photovoltaic.json -r stakeholder.json -r string.json -r unit.json -r building.json 
+ajv compile -s dsb.json -r dbe.json -r calorimetric.json -r calorimetricData.json -r common.json -r component.json -r gaps.json -r geometry.json -r hygrothermal.json -r identifier.json -r layer.json -r material.json -r method.json -r number.json -r optical.json -r opticalData -r photovoltaic.json -r stakeholder.json -r string.json -r unit.json -r building.json 
 
 PING localhost -n 10 >NUL
