e17b8058687ff63fa7fc924882d5ff005195ab0d
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2020-08-12 11:11:49 +0200

    Add meta information like name, explanation, ... to hygrothermal and photovoltaic data

--- schemas/validateDbeD04.bat
+++ schemas/validateDbeD04.bat
@@ -2,6 +2,6 @@ REM validation of dbe
 
 echo TestD04
 
-ajv compile -s dbe.json -r calorimetric.json -r calorimetricData.json -r common.json -r component.json -r gaps.json -r geometry.json -r hygrothermal.json -r identifier.json -r layer.json -r material.json -r method.json -r number.json -r optical.json -r opticalData.json -r photovoltaic.json -r stakeholder.json -r string.json -r unit.json
+ajv compile -s dbe.json -r calorimetric.json -r calorimetricData.json -r common.json -r component.json -r gaps.json -r geometry.json -r hygrothermal.json -r hygrothermalData.json -r identifier.json -r layer.json -r material.json -r method.json -r number.json -r optical.json -r opticalData.json -r photovoltaic.json -r photovoltaicData.json -r stakeholder.json -r string.json -r unit.json
 
 PING localhost -n 10 >NUL
