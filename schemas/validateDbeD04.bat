e19f36b2d35c534189ba40825de996997195cba5
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2020-08-28 15:41:23 +0200

    Remove obsolete `bat` files (in Linux just use the Bash shell, and in Windows use Git Bash that comes with Git and install GNU Make for Windows)

--- schemas/validateDbeD04.bat
+++
@@ -1,7 +0,0 @@
-REM validation of dbe
-
-echo TestD04
-
-ajv compile -s dbe.json -r calorimetric.json -r calorimetricData.json -r common.json -r component.json -r gaps.json -r geometry.json -r hygrothermal.json -r hygrothermalData.json -r identifier.json -r layer.json -r material.json -r method.json -r number.json -r optical.json -r opticalData.json -r photovoltaic.json -r photovoltaicData.json -r stakeholder.json -r string.json -r unit.json
-
-PING localhost -n 10 >NUL
--- validateExamplesD01.bat
+++
@@ -1,9 +0,0 @@
-REM validation of dbe and dsb
-
-echo TestD04
-
-PING localhost -n 1 >NUL
-
-START validateAbbreviationD01.bat
-
-PING localhost -n 3 >NUL
